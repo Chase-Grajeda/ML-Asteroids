8ece888bf7f5c12daf6209423ff9cdbeaf283b86
Chase-Grajeda <chase.grajeda@gmail.com>
2021-04-10 13:13:11 -0400

    Create bullet.py

---
+++ Code/bullet.py
@@ -0,0 +1,8 @@
+import pygame 
+import math 
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self): 
+        
+        super().__init__() 
+        self.image = pygame.image.load("Assets/LAKFJASKLFJADLKJ")
